62e3029fb39c51d75bf517644443c53bd6e454b9
tranphuongVy <22521708@gm.uit.edu.vn>
2024-11-29 22:37:27 +0700

    update

--- hotel-management/src/pages/RoomPage/RoomPage.jsx
+++ hotel-management/src/pages/RoomPage/RoomPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Sidebar from "../../components/SidebarComponent/SidebarComponent"; // Đường dẫn tùy thuộc vào cấu trúc dự án
 import styled from "styled-components";
 import RoomList from "../../components/RoomlistComponent/RoomlistComponent";
 import AddRoomForm from "../../components/AddRoomFormComponent/AddRoomFormComponent";
@@ -10,12 +9,6 @@ const LayoutWrapper = styled.div`
   display: flex;
   height: 100vh; /* Chiều cao toàn màn hình */
 `;
-
-const SidebarWrapper = styled.div`
-  width: 250px; /* Sidebar cố định chiều rộng */
-  background-color: #ffffff; /* Màu nền Sidebar */
-`;
-
 const MainContent = styled.div`
   flex: 1;
   padding: 20px;
@@ -38,12 +31,6 @@ const AddRoomFormWrapper = styled.div`
 const RoomPage = () => {
   return (
     <LayoutWrapper>
-      {/* Sidebar */}
-      <SidebarWrapper>
-        <Sidebar />
-      </SidebarWrapper>
-
-      {/* Nội dung chính */}
       <MainContent>
         {/* Room List */}
         <RoomListWrapper>
