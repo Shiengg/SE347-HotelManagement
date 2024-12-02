9cacf6b61b415b648ba538228c887587398cd82c
NhatTandayy <137869411+Shiengg@users.noreply.github.com>
2024-12-02 22:23:40 +0700

    Update somethings

--- hotel-management/src/components/HeaderComponent/style.js
+++ hotel-management/src/components/HeaderComponent/style.js
@@ -2,6 +2,6 @@ import { Row } from "antd";
 import styled from "styled-components";
 
 export const WrapperHeader = styled(Row)`
-    padding: 10px 120px;
+    padding: 10px 50px;
     background-color: #ffffff
 `
